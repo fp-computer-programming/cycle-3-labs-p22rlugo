2c9afa3435ea494923a77417c091ccce7487c761
p22rlugo <p22rlugo@fairfieldprep.org>
2021-09-28 09:59:21 -0400

    Add file

---
+++ lab_2-2.py
@@ -0,0 +1,8 @@
+# Ryan Lugo: RJL 9/28/21
+
+number_input = input("What number do you want to see if it's even?: ")
+
+if int(number_input) % 2 == 0:
+    print(number_input,"is an even number.")
+else:
+    print(number_input,"is an odd number.")
